624fa14e9b522b279a0d452020693695f842a6bc
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-09-06 01:16:45 +0200

    setup header

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
+import { FooterComponent } from '../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, FooterComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
