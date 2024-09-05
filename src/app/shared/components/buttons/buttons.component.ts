8ea145b2d1a538c083236f3bd67a31e3f2ada8c8
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-09-05 22:56:50 +0200

    various tests on buttons, to realize a smoother effect when hover ends

--- src/app/shared/components/buttons/buttons.component.ts
+++ src/app/shared/components/buttons/buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,4 +11,36 @@ import { CommonModule } from '@angular/common';
 export class ButtonsComponent {
   @Input() text: string = 'Demo-Button';
   @Input() type: 'regular' | 'link' = 'regular';
+
+  @ViewChildren('spanElement') spans!: QueryList<ElementRef>;
+
+  translateXOfLabelA: number = 0;
+  translateXOfLabelB: number = 0;
+
+  hover(event: Event) {
+    this.spans.forEach((span: ElementRef) => {
+      const style = window.getComputedStyle(span.nativeElement);
+      const matrix = style.transform;
+
+      if (matrix && matrix !== 'none') {
+        const matchedValues = matrix.match(/matrix.*\((.+)\)/);
+
+        if (matchedValues) {
+          const values = matchedValues[1].split(', ');
+          const translateX = parseFloat(values[4]);
+          console.log('Aktueller translateX-Wert:', translateX);
+        }
+
+      }
+
+    });
+
+  }
+
+  endHover(event: Event) {
+
+  }
+
+  selectLabels() {
+  }
 }
