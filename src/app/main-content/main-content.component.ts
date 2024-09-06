8d77e1da025c6556ceff764e8ad5ee7cbf775593
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-09-06 12:43:08 +0200

    added language toggle, generated section components

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
 import { FooterComponent } from '../shared/components/footer/footer.component';
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { ReferencesComponent } from "./references/references.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeroComponent, FooterComponent],
+  imports: [HeroComponent, FooterComponent, AboutMeComponent, SkillsComponent, ProjectsComponent, ReferencesComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  language: 'DE' | 'EN' = 'EN';
 }
