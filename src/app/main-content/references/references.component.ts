0c2290d7b4fe950b998a43d78646c84baf0e978b
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-09-11 10:10:10 +0200

    navigation arrows at references finished

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
 
+  returnActiveBullet(): boolean {
+    return false;
+  }
 }
