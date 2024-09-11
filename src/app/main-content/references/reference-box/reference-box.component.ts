29d0852922e853333d06f3536794c7395c89ba0e
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-09-11 22:21:16 +0200

    finished references layout

--- src/app/main-content/references/reference-box/reference-box.component.ts
+++ src/app/main-content/references/reference-box/reference-box.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { StringUtils } from '../../../utils/string-utils.utils';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reference-box',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './reference-box.component.html',
   styleUrl: './reference-box.component.scss'
 })
 export class ReferenceBoxComponent {
   @Input() referenceGiver: string = '';
+  @Input() count: number = 0;
 
   returnReferenceTranslation(prefix: string = '', suffix: string =''):string {
     const giverResult = StringUtils.cleanStr(this.referenceGiver);
--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -12,39 +12,64 @@ import { ReferenceBoxComponent } from "./reference-box/reference-box.component";
 })
 export class ReferencesComponent {
 
-  referenceGivers: string[] = [
+  referenceGivers: [string, string, string] = [
     'Phillip Sauer',
     'Phillip Sauer',
     'Phillip Sauer',
   ];
-
-  returnActiveBullet(): boolean {
-    return false;
-  }
+  
+  currentRef: number = 1;
 
   referenceId(index: number): string {
     return `reference-no${index}`;
   }
 
+  updateCurrentReference(addValue: number, oldValue: number) {
+    this.currentRef += addValue;
+    if (this.currentRef >= this.referenceGivers.length) {
+      this.currentRef = 0;
+    } else if (this.currentRef < 0) {
+      this.currentRef = this.referenceGivers.length - 1;
+    }
+    this.toggleQuotes(oldValue);
+  }
+
+  toggleQuotes(oldValue: number) {
+    const quoteImages = document.querySelectorAll('[id^="quote"]') as NodeListOf<HTMLElement>;
+    quoteImages.forEach(image => {
+      let imageId = Number(image.id.split('quote')[1]);
+      if ((this.currentRef - 2) * -1 === imageId) {
+        image.classList.add('active');
+      } else {
+        image.classList.remove('active');
+      }
+
+
+    });
+
+  }
+
   previous() {
+    this.updateCurrentReference(-1, this.currentRef);
     const references = document.querySelectorAll('[id^="reference-"]') as NodeListOf<HTMLElement>;
+    const lastRefId = references.length - 1;
     references.forEach(ref => {
       const id = Number(ref.id.split('no')[1]);
       let value = this.currentTranslateX(ref);
       value -= 100;
-      this.animateReference(ref, id, value, 'prev');
-
-      //ref.style.transform = `translateX(${value}%)`;
+      this.animateReference(ref, id, value, lastRefId, 'prev');
     });
   }
 
   next() {
+    this.updateCurrentReference(1, this.currentRef);
     const references = document.querySelectorAll('[id^="reference-"]') as NodeListOf<HTMLElement>;
+    const lastRefId = references.length - 1;
     references.forEach(ref => {
       const id = Number(ref.id.split('no')[1]);
       let value = this.currentTranslateX(ref);
       value += 100;
-      this.animateReference(ref, id, value, 'next');
+      this.animateReference(ref, id, value, lastRefId, 'next');
     });
   }
 
@@ -54,17 +79,17 @@ export class ReferencesComponent {
     return value;
   }
 
-  animateReference(ref: HTMLElement, id: number, transX: number, direction: 'prev' | 'next') {
+  animateReference(ref: HTMLElement, id: number, transX: number, lastRefId: number, direction: 'prev' | 'next') {
     const base = id + 1;
     if (direction === 'prev') {
       if (transX <= base * -100) {
-        ref.style.transform = `translate(${200 - (id * 100)}%, 0%)`;
+        ref.style.transform = `translate(${(lastRefId * 100) - (id * 100)}%, 0%)`;
       } else {
         ref.style.transform = `translate(${(transX)}%, 0%)`;
       }
     } else if (direction === 'next') {
       if (transX >= (3 - id) * 100) {
-        ref.style.transform = `translate(${-200 + ((3 - base) * 100)}%, 0%)`;
+        ref.style.transform = `translate(${-(lastRefId * 100) + ((3 - base) * 100)}%, 0%)`;
       } else {
         ref.style.transform = `translate(${(transX)}%, 0%)`;
       }
