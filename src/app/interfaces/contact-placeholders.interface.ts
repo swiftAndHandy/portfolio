9ba86c836df8c7280591a0d58895ead2d65e97b4
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-09-13 11:45:25 +0200

    layouted contacts form

---
+++ src/app/interfaces/contact-placeholders.interface.ts
@@ -0,0 +1,14 @@
+export interface ContactFormPlaceholders {
+    name: {
+        state: 'regular' | 'error';
+    },
+    email: {
+        state: 'regular' | 'error';
+    }, 
+    msg: {
+        state: 'regular' | 'error';
+    },
+    privacy: {
+        state: 'regular' | 'error';
+    }
+}
--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,14 +1,54 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { ButtonsComponent } from '../../shared/components/buttons/buttons.component';
+import { ContactFormPlaceholders } from '../../interfaces/contact-placeholders.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [TranslateModule, ButtonsComponent],
+  imports: [TranslateModule, ButtonsComponent, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  placeholders: ContactFormPlaceholders = {
+    name: {
+      state: 'regular'
+    },
+    email: {
+      state: 'regular'
+    },
+    msg: {
+      state: 'regular'
+    },
+    privacy: {
+      state: 'regular'
+    }
+  }
+
+  translatePlaceholderState(field: 'name' | 'email' | 'msg' | 'privacy') {
+    return `contact.form.placeholder.${this.placeholderState(field)}.${field}`;
+  }
+
+  placeholderState(field: 'name' | 'email' | 'msg' | 'privacy'): 'regular' | 'error' {
+    return this.placeholders[field].state;
+  }
+
+  togglePlaceholderState(field: 'name' | 'email' | 'msg' | 'privacy'){
+    switch (this.placeholders[field].state) {
+      case 'regular':
+        return this.placeholders[field].state = 'error';
+      case 'error':
+        return this.placeholders[field].state = 'regular';
+    }
+  }
+
+  resizeTextarea(event: Event) {
+    const textArea = event.target as HTMLTextAreaElement;
+    textArea.style.height = 'auto';
+    textArea.style.height = textArea.scrollHeight + 'px';
+  };
 
 }
