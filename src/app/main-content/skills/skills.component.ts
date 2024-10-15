5e4d95a5d04c20d4dd4ebfbd5f22bcfd247a2e29
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-15 18:41:04 +0200

    added a frontend/backend-toggle for skillset

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -3,15 +3,19 @@ import { TranslateModule } from '@ngx-translate/core';
 import { SingleSkillComponent } from "./single-skill/single-skill.component";
 import { ButtonsComponent } from "../../shared/components/buttons/buttons.component";
 import { Skill } from '../../interfaces/skill.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [TranslateModule, SingleSkillComponent, ButtonsComponent],
+  imports: [TranslateModule, SingleSkillComponent, ButtonsComponent, CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  displayedSkillSet: 'frontend' | 'backend' = 'frontend';
+  displayedSkillToggle: boolean = false;
+
   frontendSkills: Skill[] = [
     {'name': 'Angular'},
     {'name': 'TypeScript'},
@@ -26,6 +30,20 @@ export class SkillsComponent {
     {'name': 'Growth mindset', container: true}
   ];
 
+  backendSkills: Skill[] = [
+    {'name': 'Cloud'},
+    {'name': 'Django'},
+    {'name': 'Docker'},
+    {'name': 'Flask'},
+    {'name': 'Heroku'},
+    {'name': 'Linux'},
+    {'name': 'Postgre-SQL'},
+    {'name': 'Python'},
+    {'name': 'Redis'},
+    {'name': 'RXJS'},
+    {'name': 'SQL'},
+  ];
+
   checkContainer(skill: Skill) {
     return skill.container ? true : false;
   }
@@ -33,5 +51,16 @@ export class SkillsComponent {
   contactMe() {
     console.log('klappt');
     
+  }
+
+  toggleSkillset() {
+    switch (this.displayedSkillSet) {
+      case 'frontend':
+        this.displayedSkillSet = "backend";
+        break;
+      case 'backend':
+        this.displayedSkillSet = "frontend";
+        break;
+    }
   }
 }
