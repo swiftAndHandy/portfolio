b472e3a326642c5a8aaa762337fa0bce2d6c50f4
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-29 11:31:21 +0100

    added meta-description service
    improved SEO-Optimization and better lighthouse ratings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { MouseComponent } from "./shared/components/mouse/mouse.component";
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,18 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio';
-  
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private meta: Meta) {
     this.translate.addLangs(['de', 'en']);
     this.translate.setDefaultLang('en');
     this.translate.use(navigator.language.split('-')[0]);
+    this.setMetaDescription();
+  }
+
+  setMetaDescription() {
+    const descriptionDe = 'Erfahrener Frontend-Entwickler mit fundierten Kenntnissen in Angular, TypeScript und modernen Webtechnologien. Profitieren Sie von meiner Projekterfahrung und lassen Sie uns gemeinsam Ihre Vision verwirklichen.';
+    const descriptionEn = 'Experienced frontend developer with expertise in Angular, TypeScript, and modern web technologies. Benefit from my project experience and let’s bring your vision to life.';
+
+    const description = this.translate.currentLang === 'de' ? descriptionDe : descriptionEn;
+    this.meta.updateTag({ name: 'description', content: description });
   }
 }
